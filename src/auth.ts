f3be88bacbf9aa5a2c1597a354d59e8191f66b6a
osh3276 <osh3276@gmail.xom>
2025-09-13 13:00:12 -0400

    made database

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
 import Spotify from "next-auth/providers/spotify";
+import NeonAdapter from "@auth/neon-adapter";
+import { Pool } from "@neondatabase/serverless";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-	providers: [Spotify],
+export const { handlers, auth, signIn, signOut } = NextAuth(() => {
+	// Create a `Pool` inside the request handler.
+	const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+	return {
+		adapter: NeonAdapter(pool as any),
+		providers: [Spotify],
+	};
 });
